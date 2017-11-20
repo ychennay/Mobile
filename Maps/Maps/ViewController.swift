f87be843cbeb73ef05c21adb1079e92d1a5aed37
Yu Chen <ychennay@gmail.com>
2017-11-19 16:38:51 -0800

    maps

---
+++ Maps/Maps/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Yu Chen on 11/19/17.
+//  Copyright © 2017 Yu Chen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 34.05
+        let longitude: CLLocationDegrees = -118.42
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
